ed8b2a7edad4c09d4b038cbb14880a80c76c4ae9
John Gleason <john@gleason.technology>
2021-07-25 10:07:12 -0400

    Clean up standardized formats / add notes

--- dungeonbreakpoints.lua
+++ dungeonbreakpoints.lua
@@ -1,37 +1,37 @@
 SLASH_DUNGEONBREAKPOINTS1 = '/dbp';
 SLASH_DUNGEONBREAKPOINTS2 = '/dungeonbreakpoints';
 SlashCmdList["DUNGEONBREAKPOINTS"] = function(msg)
-    local DoS= "DoS   --> Before Last Path ~90.5% (Last area is 10%+)";
-    local HoA= "HOA   --> 79.5% before cathedral (20.5 inside (not counting gargoyles))";
-    local NW=  "NW    --> 68.2% before upstairs (31.8 Upstairs)";
+    local DoS= "DoS   --> ~90.5% Before Last ardenweald Path (Last area is 10%+)";
+    local HoA= "HOA   --> 79.5% Before cathedral (20.5 inside (not counting gargoyles))";
+    local NW=  "NW    --> 68.2% Before upstairs (31.8 Upstairs)";
     local PF=  "PF    --> 85.8% Downstairs (14.2 Downstairs)";
-    local SoA= "SOA   --> 87.4% before upstairs (12.6 upstairs)";
-    local TOP= "ToP   --> None you need to worry about";
-    local MoTS="Mists --> 10% After boss, don't pull bugs";
+    local SoA= "SOA   --> 87.4% Before upstairs (12.6 upstairs)";
+    local TOP= "ToP   --> None you need to worry about (submit suggestions!)";
+    local MoTS="Mists --> 10.0% After boss, don't pull fliers";
     local SD=  "SD    --> 74.7% If you skip garg/mistdancer pack before gauntlet";
     print("Dungeon Breakpoints:");
-    if msg == "dos" or msg == "" or msg == "de" then
+    if msg == "all" or msg == "dos" or msg == "" or msg == "de" then
         print(DoS);
     end
-    if msg == "hoa" or msg == "" or msg == "halls" then
+    if msg == "all" or msg == "hoa" or msg == "" or msg == "halls" then
         print(HoA);
     end
-    if msg == "nw" or msg == "" or msg == "necrotic" then
+    if msg == "all" or msg == "nw" or msg == "" or msg == "necrotic" then
         print(NW);
     end
-    if msg == "pf" or msg == "" or msg == "plague" then
+    if msg == "all" or msg == "pf" or msg == "" or msg == "plague" then
         print(PF);
     end
-    if msg == "soa" or msg == "" or msg == "spires" then
+    if msg == "all" or msg == "soa" or msg == "" or msg == "spires" then
         print(SoA);
     end
-    if msg == "top" or msg == "" or msg == "theater" then
+    if msg == "all" or msg == "top" or msg == "" or msg == "theater" then
         print(ToP);
     end
-    if msg == "mists" or msg == "" or msg == "mots" then
+    if msg == "all" or msg == "mists" or msg == "" or msg == "mots" then
         print(MoTS);
     end
-    if msg == "sd" or msg == "" or msg == "sanguine" then
+    if msg == "all" or msg == "sd" or msg == "" or msg == "sanguine" then
         print(SD);
     end
 end
