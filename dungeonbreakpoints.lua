455915efabb02bc82b85587cff5608f96a1608d2
John Gleason <john@gleason.technology>
2021-07-25 10:03:08 -0400

    update text

--- dungeonbreakpoints.lua
+++ dungeonbreakpoints.lua
@@ -1,15 +1,15 @@
 SLASH_DUNGEONBREAKPOINTS1 = '/dbp';
 SLASH_DUNGEONBREAKPOINTS2 = '/dungeonbreakpoints';
 SlashCmdList["DUNGEONBREAKPOINTS"] = function(msg)
-    local DoS="DoS   --> Before Last Path ~90.5% (Last area is 10%+)";
-    local HoA="HOA   --> 79.5% before cathedral (20.5 inside (not counting gargoyles))";
-    local NW="NW    --> 68.2% before upstairs (31.8 Upstairs)";
-    local PF="PF    --> 85.8% Downstairs (14.2 Downstairs)";
-    local SoA="SOA   --> 87.4% before upstairs (12.6 upstairs)";
-    local TOP="ToP   --> None you need to worry about";
+    local DoS= "DoS   --> Before Last Path ~90.5% (Last area is 10%+)";
+    local HoA= "HOA   --> 79.5% before cathedral (20.5 inside (not counting gargoyles))";
+    local NW=  "NW    --> 68.2% before upstairs (31.8 Upstairs)";
+    local PF=  "PF    --> 85.8% Downstairs (14.2 Downstairs)";
+    local SoA= "SOA   --> 87.4% before upstairs (12.6 upstairs)";
+    local TOP= "ToP   --> None you need to worry about";
     local MoTS="Mists --> 10% After boss, don't pull bugs";
-    local SD="SD    --> 74.7% If you skip garg/mistdancer pack before gauntlet";
-    print("Dungeon Break Points:");
+    local SD=  "SD    --> 74.7% If you skip garg/mistdancer pack before gauntlet";
+    print("Dungeon Breakpoints:");
     if msg == "dos" or msg == "" or msg == "de" then
         print(DoS);
     end
