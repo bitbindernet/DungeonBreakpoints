e21ae88251bc78d19400d2823725cde43ee8e5dd
John Gleason <john@gleason.technology>
2021-07-25 20:36:34 -0400

    automated build update

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
-$toctemplate = Get-Content -raw ./toc.json | convertfrom-json
+$toctemplate = Get-Content -raw ./toc.json | convertfrom-json -AsHashtable
 . ./creds.ps1
 
-$v=$($toctemplate.version)
-$toctemplate.version = [version]::New($v.Major,$($v.Minor+1),0,0)
+$v=$($toctemplate."Version")
+$toctemplate."Version" = [version]::New($v.Major,$($v.Minor+1),0,0)
 $toctemplate | convertto-json | set-content toc.json
 
 ##Generate Toc:
-$tocoutput = "## Interface: {0}`r`n## Title: {1}`r`n## Author: {2}`r`n## Version: {3}`r`n`r`n{4}" -f $($toctemplate.Interface), ($toctemplate.Title), ($toctemplate.Author), "$($toctemplate.Version)", ($toctemplate.tocfilename)
+$tocoutput = "## Interface: {0}`r`n## Title: {1}`r`n## Author: {2}`r`n## Version: {3}`r`n`r`n{4}" -f $($toctemplate.Interface), ($toctemplate.Title), ($toctemplate.Author), "$($toctemplate."Version")", ($toctemplate.packageFiles.tocfilename)
 
-Set-content -path ($toctemplate.tocFilename) -value $tocoutput -force
+Set-content -path ($toctemplate.packageFiles.tocFilename) -value $tocoutput -force
+
+Compress-Archive -Path @($tocTemplate.packageFiles.Values) -DestinationPath $toctemplate.packageFiles.luaFilename -force
