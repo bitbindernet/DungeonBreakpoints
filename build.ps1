fe39210507728d63554105dd29fda7dd794f6df4
John Gleason <john@gleason.technology>
2021-08-03 22:28:02 -0400

    update build

--- build.ps1
+++ build.ps1
@@ -11,10 +11,10 @@ $toctemplate | convertto-json | set-content $projectFile
 
 ##Generate Toc:
 $tocoutput = "## Interface: {0}`r`n## Title: {1}`r`n## Author: {2}`r`n## Version: {3}`r`n`r`n{4}" -f $($toctemplate.Interface), ($toctemplate.Title), ($toctemplate.Author), "$($toctemplate."Version")", ($toctemplate.packageFiles.luaFilename)
-
-Set-content -path ($toctemplate.packageFiles.tocFilename) -value $tocoutput -force
+new-item -path DungeonBreakpoints -ItemType Directory
+Set-content -path "./DungeonBreakpoints/($toctemplate.packageFiles.tocFilename)" -value $tocoutput -force
 
 Compress-Archive -Path @($tocTemplate.packageFiles.Values) -DestinationPath $($toctemplate.packageFiles.luaFilename).replace("lua","zip") -force
 
-. $curseApiFile
+#. $curseApiFile
 
